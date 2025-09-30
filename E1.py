73fed5b2196223053c71a7b086521129c5d4114b
A01285958 <a01285958@tec.mx>
2025-09-30 11:28:25 -0600

    Palindromos mas largos

--- E1.py
+++ E1.py
@@ -97,6 +97,64 @@ def resultados_indices(nombre_genoma, genoma, genes):
         print(f"Directo {directo if directo else "ninguno"}")
         print(f"Reverso Complementario {complemento if complemento else "ninguno"}")
 
+"""
+Encuentra el palindromo mas largo en una secuencia
+Utilizando la funcion transform para convertir los 3 genes
+Utilizando el algoritmo de Manacher para encontrar el palindromo
+"""
+def transform(s: str):
+    # Devolvemos lista (tu código ya indexa como lista)
+    texto = ['@', '$']
+    for c in s:
+        texto.append(c)
+        texto.append('$')
+    texto.append('#')
+    return texto
+
+def Manacher(gen):
+    texto = transform(gen)
+    e = len(texto)
+    P = [0] * e
+    centro, limite = 0, 0
+
+    for i in range(1, e - 1):
+        if i < limite:
+            sim = 2 * centro - i
+            P[i] = min(limite - i, P[sim])
+
+        # expandir alrededor de i
+        gap = P[i] + 1
+        # Con sentinelas, esta comparación se detiene antes de salirse
+        while texto[i - gap] == texto[i + gap]:
+            P[i] += 1
+            gap += 1
+
+        # actualizar centro y límite
+        if i + P[i] > limite:
+            limite = i + P[i]
+            centro = i
+
+    # localizar mejor centro (ojo: NO reutilizar la i anterior)
+    best_i = 1
+    best_len = 0
+    for k in range(1, e - 1):
+        if P[k] > best_len:
+            best_len = P[k]
+            best_i = k
+
+    # índices en la cadena original
+    start = (best_i - best_len) // 2
+    end = start + best_len - 1
+    return start, end
+
+def resultados_palindromos(genes: dict):
+    orden = ["Gen M", "Gen S", "Gen ORF1ab"]
+    for nombre in orden:
+        sec = genes[nombre]
+        i, j = Manacher(sec)
+        pal = sec[i:j+1] if j >= i >= 0 else ""
+        print(f"{nombre}: longitud={len(pal)}, palindromo='{pal}'")
+
 def main():
     genomas = {
         "Wuhan_2019": "SARS-COV-2-MN908947.3.txt",
@@ -115,6 +173,8 @@ def main():
 
     #Reporte para Wuhan
     resultados_indices("Wuhan_2019", secuencias_genomas["Wuhan_2019"], secuencias_genes)
+    resultados_palindromos(secuencias_genes)
+    
 
 if __name__ == "__main__":
     main()
